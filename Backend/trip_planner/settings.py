f18fe4e06f748299fdc39cfacf4670d85ce88bf1
Thomas-m-waka <thomasmmuteti4@gmail.com>
2025-05-20 12:53:46 +0300

    add cors allow origins

--- Backend/trip_planner/settings.py
+++ Backend/trip_planner/settings.py
@@ -187,8 +187,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  
-    
+    "https://eld-trip-logger.vercel.app",
 ]
 
 #CORS_ALLOW_CREDENTIALS = True  
